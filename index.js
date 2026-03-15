652631241046758abb2be7db717086cc23d51be2
Radhe Krishna <SAFITECH@RajkaransTechOfficial.Rajkarans_Tech>
2026-03-15 16:09:38 +0530

    fix render port issue

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const mongoose = require("mongoose");
 const app = require("./app");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-mongoose.connect("mongodb+srv://testuser:Akanksha@cluster1.mwdwdyi.mongodb.net/youtubeSubscribersDB?retryWrites=true&w=majority")
+mongoose.connect("your_mongodb_connection_string")
 
-.then(()=>{
-console.log("MongoDB Connected");
+.then(() => {
+  console.log("MongoDB Connected");
 
-app.listen(PORT, ()=>{
-console.log(`Server running on port ${PORT}`);
-});
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 
 })
 
-.catch(err=>{
-console.log(err);
+.catch(err => {
+  console.log(err);
 });
